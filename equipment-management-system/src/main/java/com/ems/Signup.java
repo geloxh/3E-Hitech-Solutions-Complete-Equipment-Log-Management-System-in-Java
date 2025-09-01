d753eb4d759dbb2ea2afc149667362a68eb747cb
Angelo <ace.blazado7@gmail.com>
2025-09-01 16:34:59 +0800

    Use Maven Project For the EMS

---
+++ equipment-management-system/src/main/java/com/ems/Signup.java
@@ -0,0 +1,15 @@
+package com.ems;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JDesktopPane;
+import javax.swing.JOptionPane;
+
+/**
+ * @author geloxh
+ */
+
+public class Signup {
+    
+}
